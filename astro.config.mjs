12c641ee617c1b7bc52b339be19e20fc5594e8c7
eitaar <154091730+eitaar@users.noreply.github.com>
2025-03-12 19:19:39 +0000

    dscss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   // ...
   integrations: [vue()],
+  output:'server',
+  adapter: cloudflare(),
 });
