6ca7a45cdf53846f602999806601e21f9e4587eb
eitaar <154091730+eitaar@users.noreply.github.com>
2025-03-12 19:57:40 +0000

    fgva

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 import vue from '@astrojs/vue';
 
 import cloudflare from '@astrojs/cloudflare';
@@ -8,4 +9,7 @@ export default defineConfig({
   integrations: [vue()],
   output:'server',
   adapter: cloudflare(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
