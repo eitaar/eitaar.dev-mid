bfa204ee71457cecb751288302655511155b2d49
eitaar <154091730+eitaar@users.noreply.github.com>
2025-03-26 19:28:48 +0000

    bg photo add

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,8 +1,15 @@
 <template>
-  <div class="TOP w-7/10 h-screen [clip-path:polygon(0_0,100%_0,65%100%,_0%100%);] bg-base-300">
+  <div class="bg-base-100">
+    <div class="TOP w-4/10 h-screen [clip-path:polygon(0_0,100%_0,65%_0100%,_0%100%)] bg-base-300">
     <div class="h-screen w-1/2 ml-[2.5vw]">
-      <h1 class="text-[10vh] text-left font-bold font-Quantico tracking-tightest p-0 text-base-content">eitaar.dev</h1>
+      <h1 class="text-[5vw] text-left font-bold font-Quantico tracking-tightest p-0 text-base-content">eitaar.dev</h1>
+      <div class="text-[2.5vw] text-left font-bold font-Quantico text-base-content pt-5">
+        <a class="link PROJECTS">Projects</a><br/>
+        <a class="link">Blog</a><br/>
+        <a class="link">About</a>
+      </div>
     </div>
+  </div>
   </div>
   <div class="flex gap-2 fixed top-0 right-0 h-[10vmin] items-center mr-[2vmin]">
         <!--Navigation links-->
@@ -25,9 +32,4 @@
 <script setup>
 import Sun from '../components/svg/sun.vue';
 import Moon from '../components/svg/moon.vue';
-import {gsap} from 'gsap';
-import {onMounted} from 'vue';
-onMounted(()=>{
-  gsap.from('.TOP',{duration:1,x:-100});
-})
 </script>
