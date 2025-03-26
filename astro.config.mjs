c7a21e5ffba2e4b4b56815f4b20c3947bb4cebe5
eitaar <154091730+eitaar@users.noreply.github.com>
2025-03-26 20:15:15 +0000

    bruh

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import vue from '@astrojs/vue';
-import cloudflare from '@astrojs/cloudflare';
+//import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   // ...
   integrations: [vue(),icon()],
-  output:'server',
-  adapter: cloudflare(),
+  //output:'server',
+  //adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
   },
