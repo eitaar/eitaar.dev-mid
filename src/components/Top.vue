2f557b01a8c2f830d973f2aec9f6751148cb9944
eitaar <154091730+eitaar@users.noreply.github.com>
2025-04-17 18:50:07 +0900

    made rly simple phone version

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="bg-neutral h-screen">
-    <div class="TOP w-4/10 h-screen [clip-path:polygon(0_0,100%_0,65%_0100%,_0%100%)] bg-base-300 z-10">
+    <div class="TOP w-full h-1/3 lg:w-4/10 lg:h-screen lg:[clip-path:polygon(0_0,100%_0,65%_0100%,_0%100%)] bg-base-300 z-10">
       <div class="h-screen w-1/2 ml-[2.5vw]">
-        <h1 class="text-[5vw] text-left font-bold font-Quantico tracking-tightest p-0 text-base-content">eitaar.dev</h1>
+        <h1 class="text-[15vw] lg:text-[5vw] pt-3 text-left font-bold font-Quantico tracking-tightest p-0 text-base-content">eitaar.dev</h1>
         <div class="text-[2.5vw] text-left font-bold font-Quantico text-base-content pt-5">
           <slot name="AboutLink" />
           <slot name="ProjectLink"/>
-          <p class="link">Blog</p>
+          <p class="link text-3xl lg:text-5xl">Blog</p>
         </div>
       </div>
     </div>
